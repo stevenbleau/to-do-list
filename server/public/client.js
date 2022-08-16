29f557f4aacb2d5a20c12f9bcab01a0af68f02ca
Steven Bleau <bleausd@gmail.com>
2022-08-16 11:51:29 -0500

    added buttons

--- server/public/client.js
+++ server/public/client.js
@@ -4,4 +4,34 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('Jquery sourced')
+
+    //click event listeners
+    $('body').on('click', '#submitBtn', submitTask);
+    $('body').on('click','#completeBtn', completeTask);
+    $('body').on('click','#deleteBtn', deleteTask);
+}
+
+
+
+/**
+ * Subit Task
+ */
+function submitTask(){
+    console.log('in submitTask');
+}
+
+
+/**
+ * Complete Task
+ */
+function completeTask(){
+    console.log('in completeTask');
+}
+
+
+/**
+ * Delete Task
+ */
+function deleteTask(){
+    console.log('in deleteTask');
 }
